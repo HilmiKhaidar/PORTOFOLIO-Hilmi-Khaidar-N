1c4ace0c5ef8565b3aaf8ae15e8fd14908a3b208
HilmiKhaidar <Hilmikhaidarnabilla@gmail.com>
2025-05-12 14:30:22 -0700

    Mencoba buat file beranda

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman Beranda</title>
+</head>
+<body>
+    <h1>Halo, ini halaman beranda!</h1>
+    <p>Laravel-nya sudah berhasil diubah dari tampilan default 🎉</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('beranda'); // <--- ini nama file .blade.php nanti
 });
