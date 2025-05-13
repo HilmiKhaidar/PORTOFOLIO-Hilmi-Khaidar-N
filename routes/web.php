edfdf2bd1b21c8becd93015eaf541884e1f85d47
HilmiKhaidar <Hilmikhaidarnabilla@gmail.com>
2025-05-12 22:09:22 -0700

    ngulik beberapa

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BerandaController;
 
-Route::get('/', [BerandaController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
